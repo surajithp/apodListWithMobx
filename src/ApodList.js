68c814f4b9844725358397c779f89677f775ff33
surajith <surajith1991@gmail.com>
2019-01-06 15:57:14 +0530

    Added mobx

--- src/ApodList.js
+++ src/ApodList.js
@@ -2,144 +2,67 @@ import React, { Component } from "react";
 import {
   Text,
   View,
+  Button,
   FlatList,
-  TouchableOpacity,
   Image,
-  Dimensions,
-  ActivityIndicator,
-  Alert
+  TouchableOpacity
 } from "react-native";
+import { observer } from "mobx-react";
+import { observable, computed, action, autorun } from "mobx";
 import Apidatamanager from "./DataManager";
-import glamorous, { ThemeProvider } from "glamorous-native";
-const dimensions = Dimensions.get("window");
-const Indicatorview = glamorous.view({
-  flex: 1,
-  justifyContent: "center"
-});
-const Mainview = glamorous.view({
-  height: dimensions.height - 20,
-  width: dimensions.width
-});
+//import glamorous, { ThemeProvider } from "glamorous-native";
+@observer
 export default class ApodList extends Component {
-  state = {
-    data: [],
-    start_date: null,
-    end_date: null,
-    isdatafetching: true,
-    initialrender: false
-  };
+  @observable start_date: null;
+  @observable end_date: null;
+
   componentWillMount = () => {
+    console.log("==============");
     let moment = require("moment");
-    const enddate = moment().subtract(1, "days").format("YYYY-MM-DD");
-    const startdate = moment()
-      .subtract(9, "days")
+    this.end_date = moment().format("YYYY-MM-DD");
+    this.start_date = moment()
+      .subtract(11, "days")
       .format("YYYY-MM-DD");
-    Apidatamanager.getdata(
-      startdate,
-      enddate,
-      this.handleResponse,
-      this.apiResponsefailure
-    );
-    this.setState({ start_date: startdate, end_date: enddate });
+    Apidatamanager.fetchData(this.start_date, this.end_date);
   };
-
-  handleResponse = response => {
-    console.log(response);
-    this.setState({
-      data: response.data.reverse(),
-      isdatafetching: false,
-      initialrender: !this.state.initialrender
-    });
-  };
-
-  apiResponsefailure = err => {
-    this.setState({ isdatafetching: false });
-    Alert.alert("Sorry, unable to fetch data.");
-  };
-  onPressLoadMore = () => {
-    const previousstartdate = this.state.start_date;
-    let moment = require("moment");
-    const enddate = moment(`${previousstartdate}`)
-      .subtract(1, "days")
-      .format("YYYY-MM-DD");
-    const startdate = moment(`${enddate}`)
-      .subtract(9, "days")
-      .format("YYYY-MM-DD");
-    Apidatamanager.getdata(
-      startdate,
-      enddate,
-      this.handlebuttonResponse,
-      this.apiResponsefailure
-    );
-    this.setState({ start_date: startdate, end_date: enddate });
-  };
-
-  handlebuttonResponse = response => {
-    const responsedata = response.data;
-    responsedata.reverse();
-    this.setState(prevState => ({
-      data: prevState.data.concat(responsedata),
-      isdatafetching: false
-    }));
-  };
-
-  //  showIndicator = condition => {
-  //    this.setState({ isdatafetching: condition });
-  //  };
-
-  _footerComponent = () => {
-    if (!this.state.isdatafetching)
-      return (
-        <Indicatorview>
-          <ActivityIndicator size="large" color="#0000ff" />
-        </Indicatorview>
-      );
-    else {
-      return null;
-    }
-  };
-
   _keyExtractor = (item, index) => item.date;
 
-  _renderItem = ({ item }) => (
-    <View>
-      <TouchableOpacity onPress={this._onPressButton}>
-        <Image
-          style={{ width: 100, height: 100 }}
-          source={{
-            uri: item.url
-          }}
-        />
-      </TouchableOpacity>
-      <Text>{item.title}</Text>
-      <Text>{item.date}</Text>
-    </View>
-  );
-
+  _renderItem = observer(({ item }) => {
+    return (
+      <View>
+        <TouchableOpacity onPress={this._onPressButton}>
+          <Image
+            source={{
+              uri: item.url
+            }}
+          />
+        </TouchableOpacity>
+        <Text>{item.title}</Text>
+        <Text>{item.date}</Text>
+      </View>
+    );
+  });
   render() {
-    console.log(this.state.data);
-    console.log(this.state.isdatafetching);
-    let activityindicator;
-    if (
-      this.state.isdatafetching === true &&
-      this.state.initialrender === false
-    ) {
-      activityindicator = <ActivityIndicator size="large" color="#0000ff" />;
+    console.log(Apidatamanager.data.slice());
+    let list;
+    if (Apidatamanager.data.slice().length > 0) {
+      <FlatList
+        data={Apidatamanager.data.slice()}
+        keyExtractor={this._keyExtractor}
+        renderItem={this._renderItem}
+      />;
     } else {
-      activityindicator = null;
+      list = (
+        <View>
+          <Text>No Images Loaded</Text>
+        </View>
+      );
     }
     return (
-      <Mainview>
-        <Indicatorview>{activityindicator}</Indicatorview>
-        <FlatList
-          data={this.state.data}
-          keyExtractor={this._keyExtractor}
-          renderItem={this._renderItem}
-          onEndReachedThreshold={0.1}
-          onEndReached={this.onPressLoadMore}
-          ListFooterComponent={this._footerComponent}
-        />
-      </Mainview>
+      <View>
+        <View>{list}</View>
+        <Text>Scroll down to get more Images</Text>
+      </View>
     );
   }
 }
--- src/DataManager.js
+++ src/DataManager.js
@@ -1,17 +1,33 @@
 import axios from "axios";
+import { action, observable } from "mobx";
 class DataManager {
   // constructor(date1, date2, handleResponse) {
   //   this.startdate = date1;
   //   this.enddate = date2;
   //   this.handleResponse = handleResponse;
   // }
-  getdata(startdate, enddate, successfn, failurefn) {
+
+  @observable data = [];
+  @observable state = "pending";
+  @action
+  fetchData(startdate, enddate) {
+    console.log(startdate)
+    console.log(enddate)
     axios
       .get(
         `https://api.nasa.gov/planetary/apod?api_key=YPnh5fLrnPlqbVeCN86tba4qEEqrh9DrlLgkphhS&start_date=${startdate}&end_date=${enddate}`
       )
-      .then(response => successfn(response))
-      .catch(err => failurefn(err));
+      .then(
+        response => {
+          console.log(response);
+          const responsedata = response.data;
+          this.data = responsedata;
+          this.state = "done";
+        },
+        error => {
+          console.log(error);
+        }
+      );
   }
 }
 
