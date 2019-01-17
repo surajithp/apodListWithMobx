48b1fc8bb279d882ac9f77a1039a858469f0e5e6
surajith <surajith1991@gmail.com>
2019-01-17 14:39:05 +0530

    Added two columns to display Images and used react navigation to display full Image with explanation

--- App.js
+++ App.js
@@ -1,26 +1,60 @@
 import React from "react";
 import { Button, View, Text, Image } from "react-native";
-import { createStackNavigator, createAppContainer } from "react-navigation"; // Version can be specified in package.json
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import glamorous, { ThemeProvider } from "glamorous-native";
 import ApodList from "./src/ApodList";
+
+const FullImageview = glamorous.view({
+  flex: 1
+});
+const Explanationview = glamorous.view({
+  position: "absolute",
+  bottom: 0
+});
+const Dateview = glamorous.view({
+  position: "absolute",
+  top: 0,
+  right: 0
+});
+const ExplanationText = glamorous.text({
+  color: "white"
+});
+const TitleText = glamorous.text({
+  color: "white",
+  fontSize: 20,
+  fontWeight: "bold"
+});
 
 class FullImage extends React.Component {
   render() {
     const { navigation } = this.props;
     const urlImage = navigation.getParam("imageUrl", "Image Not loaded");
-    const explanationImage = navigation.getParam(
-      "imgexplanation",
+    const explanationOfImage = navigation.getParam(
+      "imgExplanation",
       "Image Explanation "
     );
+    const imagedate = navigation.getParam(
+      "imageDate",
+      "Date of Particular Iamge"
+    );
     return (
-      <View>
+      <FullImageview>
         <Image
-          style={{ width: 300, height: 500 }}
+          style={{ flex: 1 }}
           source={{
             uri: urlImage
           }}
         />
-        <Text>{JSON.stringify(explanationImage)}</Text>
-      </View>
+        <Explanationview>
+          <ExplanationText>
+            <TitleText>Title Explanation:</TitleText>
+            {explanationOfImage}
+          </ExplanationText>
+        </Explanationview>
+        <Dateview>
+          <ExplanationText>{imagedate}</ExplanationText>
+        </Dateview>
+      </FullImageview>
     );
   }
 }
--- src/ApodList.js
+++ src/ApodList.js
@@ -15,18 +15,43 @@ import { observable } from "mobx";
 import Apidatamanager from "./DataManager";
 import { createStackNavigator, createAppContainer } from "react-navigation";
 const dimensions = Dimensions.get("window");
+console.log(dimensions.width);
+console.log(dimensions.height);
+console.log(Dimensions.get("screen").height);
 const Mainview = glamorous.view({
-  height: dimensions.height - 70,
+  height: dimensions.height - 60,
   width: dimensions.width
 });
 const Indicatorview = glamorous.view({
   flex: 1,
   justifyContent: "center"
 });
+const Renderview = glamorous.view({
+  flex: 1,
+  padding: 10
+});
+const Titleview = glamorous.view({
+  position: "absolute",
+  bottom: 20,
+  justifyContent: "center"
+});
+const Dateview = glamorous.view({
+  position: "absolute",
+  top: 0,
+  right: 10
+});
+const ImageView = glamorous.view({
+  flex: 1,
+  height: dimensions.height / 4,
+  width: dimensions.width / 2 - 20
+});
+const Textcolor = glamorous.text({
+  color: "white"
+});
 @observer
 export default class ApodList extends Component {
   static navigationOptions = {
-    title: "APOD Images"
+    title: "APOD"
   };
 
   isDataFetched = "";
@@ -44,10 +69,10 @@ export default class ApodList extends Component {
   _keyExtractor = (item, index) => item.date;
 
   _onTouchingImage = item => {
-    console.log("Image clicked");
     this.props.navigation.navigate("Imagescreen", {
       imageUrl: item.url,
-      imgexplanation: item.explanation
+      imgExplanation: item.explanation,
+      imageDate: item.date
     });
   };
 
@@ -64,17 +89,25 @@ export default class ApodList extends Component {
   };
 
   _renderItem = ({ item }) => {
+    let moment = require("moment");
+    let date = moment(item.date).format("MMM Do YYYY");
     return (
-      <View>
-        <TouchableOpacity onPress={() => this._onTouchingImage(item)}>
-          <Image
-            style={{ width: 100, height: 100 }}
-            source={{ uri: item.url }}
-          />
-        </TouchableOpacity>
-        <Text>{item.title}</Text>
-        <Text>{item.date}</Text>
-      </View>
+      <Renderview>
+        <ImageView>
+          <TouchableOpacity onPress={() => this._onTouchingImage(item)}>
+            <Image
+              style={{ height: 175, width: 175 }}
+              source={{ uri: item.url }}
+            />
+          </TouchableOpacity>
+          <Titleview>
+            <Textcolor>{item.title}</Textcolor>
+          </Titleview>
+          <Dateview>
+            <Textcolor>{date}</Textcolor>
+          </Dateview>
+        </ImageView>
+      </Renderview>
     );
   };
   render() {
@@ -92,7 +125,9 @@ export default class ApodList extends Component {
           data={Apidatamanager.data.slice()}
           keyExtractor={this._keyExtractor}
           renderItem={this._renderItem}
-          onEndReachedThreshold={0.2}
+          horizontal={false}
+          numColumns={2}
+          onEndReachedThreshold={0.1}
           onEndReached={this.onScrollingLoadMore}
           ListFooterComponent={this._footerComponent}
         />
